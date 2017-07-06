b35497ee28753750a05fbf00c52060ea2949c905
Hugo Samayoa <h3samayoa@gmail.com>
2017-07-05 17:08:05 -0700

    Added third todo

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Send your homework email!',
+	completed: false
+}, {
+	id: 2,
+	description: 'Hang out with friends',
+	completed: false
+}, 
+	id: 3, 
+	description: 'Wake up''
+	completed: true
+];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
+
+// Get /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+// Get /todos/:id
 
 app.listen(PORT, function() {
 	console.log('Express listening on port '+ PORT + '!');
