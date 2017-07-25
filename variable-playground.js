24f5d25c22842e509125eb85c55abd3d50a385b3
Hugo Samayoa <h3samayoa@gmail.com>
2017-07-24 17:39:49 -0700

    Add  variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Hugo',
+// 	age: 14
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Hugo',
+// 	// 	age: 15
+// 	// };
+// 	obj.age = 15;
+// }
+
+// updatePerson(person);
+// console.log(person)
+
+// Array Example
+var grades = [21, 99];
+
+function addGrades (gradesArr) {
+	gradesArr.push(68); 
+	debugger;
+
+	//gradesArr = [12, 33, 99 ]
+}
+
+addGrades(grades);
+console.log(grades);
