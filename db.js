8c9ccb69d0d0d8deee5764a16e3c2c09c1bfae8e
Hugo Samayoa <htplbc@gmail.com>
2018-01-03 16:33:30 -0800

    add debug log

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
+	console.log(process.env.DATABASE_URL);
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: 'postgres', 
+		dialect: 'postgres',
 		dialectOptions: {
-			native: true, 
-			ssl: true 
+			native: true,
+			ssl: true
 		}
 	});
 } else {
