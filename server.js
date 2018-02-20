2db7083aa068697ec4a59bfe180523df701b4553
Hugo Samayoa III <hugosamayoa3@hugos-mbp.lan>
2018-02-19 17:16:14 -0800

    done

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
+
 
 app.use(bodyParser.json());
 
@@ -15,10 +17,12 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-// Get /todos?completed=false&q=work
-app.get('/todos', function(req, res) {
+// GET /todos?completed=false&q=work
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -36,16 +40,21 @@ app.get('/todos', function(req, res) {
 		where: where
 	}).then(function(todos) {
 		res.json(todos);
-	}).then(function(e) {
+	}, function(e) {
 		res.status(500).send();
 	});
 });
 
-// Get /todos/:id
-app.get('/todos/:id', function(req, res) {
+// GET /todos/:id
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -53,43 +62,48 @@ app.get('/todos/:id', function(req, res) {
 		}
 	}, function(e) {
 		res.status(500).send();
-	})
+	});
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
-	var body = _.pick(req.body, 'description', 'completed'); // Use _.pick to only description and completed 
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then(function () {
+			return todo.reload();
+		}).then(function (todo) {
+			res.json(todo.toJSON());
+		});
 	}, function(e) {
 		res.status(400).json(e);
 	});
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
 			res.status(404).json({
-				error: 'no todo found with that id'
+				error: 'No todo with id'
 			});
 		} else {
 			res.status(204).send();
 		}
-	}, function(e) {
+	}, function() {
 		res.status(500).send();
 	});
 });
 
-// PUT/todos/:id
-app.put('/todos/:id', function(req, res) {
+// PUT /todos/:id
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
@@ -102,7 +116,12 @@ app.put('/todos/:id', function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -127,13 +146,22 @@ app.post('/users', function (req, res) {
 	});
 });
 
-// POST /users/login 
+// POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
-		res.json(user.toPublicJSON());
-	}, function (e) {
+		var token = user.generateToken('authentication');
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function (e) {
+		console.log('e', e);
 		res.status(401).send();
 	});
 });
